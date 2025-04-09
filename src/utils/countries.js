8ab700a3df0c4c49acf2a14eeda7500375f0199e
Jaden Thompson <jadenthompson@Jadens-MacBook-Pro.local>
2025-04-09 01:58:43 +0100

    Final onboarding + homepage polish

---
+++ src/utils/countries.js
@@ -0,0 +1,16 @@
+const countries = [
+    { name: 'United States', code: 'US' },
+    { name: 'United Kingdom', code: 'GB' },
+    { name: 'Canada', code: 'CA' },
+    { name: 'Australia', code: 'AU' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'France', code: 'FR' },
+    { name: 'Spain', code: 'ES' },
+    { name: 'Japan', code: 'JP' },
+    { name: 'South Korea', code: 'KR' },
+    { name: 'Brazil', code: 'BR' },
+    // ... add full list or install from npm later
+  ];
+  
+  export default countries;
+  
